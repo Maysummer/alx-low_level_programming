bb7b1b8084396a0de6df0af1ceebebea4ff19aa9
Maysummer <udojyk@gmail.com>
2022-03-10 13:58:01 -0800

    print using printf in betty style and documentation

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Beginning of the function in betty style and documentation
+ *
+ * Return> End of function
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
