644b5f417c6eb03edd9d64745e77a4d736169c1f
Maysummer <udojyk@gmail.com>
2022-03-10 15:34:53 -0800

    print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Beginning of the function in betty style and documentation
+ *
+ * Return: End of function
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
