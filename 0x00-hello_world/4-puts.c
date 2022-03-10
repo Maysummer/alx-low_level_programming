26c1ca3a142e3a6713a63e0ad6de3ff8dda7d3ca
Maysummer <udojyk@gmail.com>
2022-03-10 13:33:21 -0800

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+ * hello_puts - Printing a string using puts and betty style and documentation
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
