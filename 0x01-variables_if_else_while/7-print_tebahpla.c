9738cce7c84f540b56ff1b6aac93e37b0532a27c
Maysummer <udojyk@gmail.com>
2022-03-11 12:40:22 -0800

    print letters backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Beginning of the function in betty style and documentation
+ *
+ * Return: End of function
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
