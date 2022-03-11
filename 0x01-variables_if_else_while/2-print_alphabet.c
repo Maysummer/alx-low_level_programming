bf7a5bd96730c5ffef7a3bf83a2cc27f8efa3373
Maysummer <udojyk@gmail.com>
2022-03-11 05:20:23 -0800

    print alphabets in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,12 @@
  */
 int main(void)
 {
-	int ch;
-
-	clrscr();
+	char ch;
 	
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
-		putchar ('\n');
-		getch();
 	}
+	putchar('\n')
 	return (0);
 }
