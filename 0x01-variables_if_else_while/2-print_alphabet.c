6ad6b0248523ac146b5505e5aa0e03533443dbb4
Maysummer <udojyk@gmail.com>
2022-03-11 04:58:27 -0800

    print alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Beginning of the function in betty style and documentation
+ *
+ * Return: End of function
+ */
+int main(void)
+{
+	char ch = "a";
+
+	for (ch = "a"; ch <= "z"; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
