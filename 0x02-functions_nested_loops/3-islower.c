2c33ee46b1ea623a6da4541fa1ce8a63bba7bc73
Maysummer <udojyk@gmail.com>
2022-03-15 06:50:35 -0700

    check if a letter is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _is lower - check if a letter is in lowercase
+ * Return: Always 0 (success)
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
