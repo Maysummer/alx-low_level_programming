46597dadb1b06e2bc8f4ef49d38f426f35190393
Maysummer <udojyk@gmail.com>
2022-03-19 03:05:03 -0700

    print 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,7 +4,7 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i <= 9; i++)
 	{
@@ -16,6 +16,6 @@ void more_numbers(void)
 			_putchar((j % 10) + '0');
 			}
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
