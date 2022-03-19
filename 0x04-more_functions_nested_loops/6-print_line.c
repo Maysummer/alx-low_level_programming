a0783d205755486ec30f8c0b26f76e45cd4e86b9
Maysummer <udojyk@gmail.com>
2022-03-19 05:14:44 -0700

    draw a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * draw a straight line in the terminal.
+ * @n: number of times '_' should be printed.
+ */
+void print_line(int n)
+{
+	int n, i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar('_');
+	}
+}
