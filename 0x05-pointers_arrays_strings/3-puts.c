ab6f30838a6ae9bee472097d36beba4e7b593729
Maysummer <udojyk@gmail.com>
2022-03-21 08:07:57 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - print a string
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar('str[i]);
+		i++;
+	}
+}
