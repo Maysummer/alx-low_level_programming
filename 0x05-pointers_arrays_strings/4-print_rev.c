caf4f2f07e0085200894274a39bc8893679cd336
Maysummer <udojyk@gmail.com>
2022-03-21 09:05:28 -0700

    print a string in reverse order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,7 @@ void print_rev(char *s)
 
 	i = 0;
 
-	while (s[i] != '\0')
+	while (!(s[i]))
 	{
 		i++;
 	}
@@ -19,7 +19,7 @@ void print_rev(char *s)
 
 	while (j >= 0)
 	{
-		_putchar(j);
+		_putchar(s[j]);
 		j--;
 	}
 	_putchar('\n');
