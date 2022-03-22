0cfe2e79c9d755c0a6b6942453f44c5a68eabc77
Maysummer <udojyk@gmail.com>
2022-03-22 02:04:03 -0700

    copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcpy - copy the string
+ * @dest: destination
+ * @src: source
+ * Return: return the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, j, len;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	len = i;
+
+	for (j = 0; j < len; j++)
+	{
+		dest[j] =  src [j];
+	}
+	dest[j] = '\0';
+	return (dest);
+}
