b55e32a2562638f7d089046bbafb84200676d99a
Maysummer <udojyk@gmail.com>
2022-03-23 11:26:49 -0700

    concatenate two string to a specified point

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings to a specified point
+ * @dest: destination
+ * @src: source
+ * @n: limit to be copied
+ * Return: pointer to the string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++
+	}
+
+	j = 0;
+
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
+}
