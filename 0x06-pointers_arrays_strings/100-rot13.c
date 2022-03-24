0ae4c07544716049ea27d0df3f6683614cacad6f
Maysummer <udojyk@gmail.com>
2022-03-24 07:58:41 -0700

    encode with rot-13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,14 +9,14 @@ char *rot13(char *s)
 {
 	int i, j;
 
-	char *a = "ABCDEFGHIJKLMONPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	i = 0;
 	
 	while (s[i] != '\0')
 	{
-		for (j = 0; j < 52; j++)
+		for (j = 0; j <= 52; j++)
 		{
 			if (s[i] == a[j])
 			{
