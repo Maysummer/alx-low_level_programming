dfdab2d5c83d0667ecdd5dac10f223e24a590e40
Maysummer <udojyk@gmail.com>
2022-03-24 07:50:45 -0700

    encode with rot-13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - eencode with rot 13
+ * @s: string
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char *a = "ABCDEFGHIJKLMONPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	i = 0;
+	
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
