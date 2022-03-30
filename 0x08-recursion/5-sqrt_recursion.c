4d12815ec470dd5fc1585dc0247aed001c6cfa49
Maysummer <udojyk@gmail.com>
2022-03-30 14:25:19 -0700

    return the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: number
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (find_sqrt(n, 0));
+}
+
+int find_sqrt(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	return (find_sqrt(n, i + 1));
+}
