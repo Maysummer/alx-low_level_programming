982435bde4d505846148869430ae02d7e5eaceb2
Maysummer <udojyk@gmail.com>
2022-03-30 12:08:29 -0700

    return the factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+/**
+ * factorial - return the factorial of a number
+ * @n: number
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
