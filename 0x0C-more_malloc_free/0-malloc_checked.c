3ed62977de1fb314ea6df9cceba1b0bb56a1a820
Maysummer <udojyk@gmail.com>
2022-04-08 13:26:29 -0700

    allocate space

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: amount of bytes to be allocated
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
