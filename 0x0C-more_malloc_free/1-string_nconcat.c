9874a26f99cd7da50aefd54e7b96e68a702b7a15
Maysummer <udojyk@gmail.com>
2022-04-09 07:04:29 -0700

    concat two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		newStr[i] = s1[i];
 	}
-	for (j = 0; j < len2 && j < n; j++)
+	for (j = 0; s2[j] && j < n; j++)
 	{
 		newStr[i] = s2[j];
 	}
