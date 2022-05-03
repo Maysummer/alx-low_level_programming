ba8770d981cffb8f63dda28be948c71500054338
Maysummer <udojyk@gmail.com>
2022-05-03 05:17:51 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, fdw;
+	int fd, fdw, s;
+
+	s = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -17,13 +19,21 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		open(filename, O_CREAT, 0600);
 
-	fd = open(filename, O_CREAT | O_EXCL | O_TRUNC | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	fdw = write(fd, text_content, strlen(text_content));
-	if (fdw == -1)
-		return (-1);
+	if (text_content)
+	{
+		while (text_content[s])
+			s++;
+		fdw = write(fd, text_content, s);
+		if (fdw != s)
+			return (-1);
+	}
+
+	close(fd);
+
 	return (1);
 }
