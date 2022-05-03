174388babd314f1019431e770d973f8a948aa9a3
Maysummer <udojyk@gmail.com>
2022-05-03 16:30:42 -0700

    copy the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - copy content of a file to another
+ * @argc: argument count
+ * @argv:argument vector
+ * Return: 1 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	int fd, fdw, fdc, fdwc;
+	char *buf;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+		return (-1);
+
+	read(fd, buf, 1024);
+
+	fdw = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fdw == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	fdc = close(fd);
+	fdwc = close(fdw);
+	if (fdc == -1 || fdwc == -1)
+	{
+		if (fdc == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		if (fdwc == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdw);
+		exit(100);
+	}
+	return (0);
+}
