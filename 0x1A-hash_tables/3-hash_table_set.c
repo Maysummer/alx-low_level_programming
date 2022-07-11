adfae30ec248336c619e40228acf8cc1f04f3d56
Maysummer <udojyk@gmail.com>
2022-07-10 22:43:08 -0700

    add to a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,25 +9,30 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_elem, *temp;
+	hash_node_t *new_elem, *index_list;
 	unsigned long int index;
+
+	new_elem = malloc(sizeof(hash_node_t));
+        if (!new_elem)
+                return (0);
 
 	if (!ht || !key || !value || strcmp(key, ""))
 		return (0);
 
-	new_elem = malloc(sizeof(hash_node_t));
-	if (!new_elem)
-		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+        index_list = ht->array[index];
+
+	while (index_list)
+	{
+		if (strcmp(index_list->key, key) == 0)
+		{
+			index_list->value = strdup(value)
+		}
+		index_list = index_list->next;
+	}
 
 	new_elem->key = strdup(key);
 	new_elem->value = strdup(value);
-	new_elem->next = NULL;
-
-	index = key_index((const unsigned char *)key, ht->size);
-	temp = ht->array[index];
-
-	if (temp != NULL)
-		temp = temp->next;
 	new_elem->next = ht->array[index];
 	ht->array[index] = new_elem;
 	return (1);
