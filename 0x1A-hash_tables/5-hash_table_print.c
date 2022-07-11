d9608d0611fde205d34ac3ab89e0f39b23982b1f
Maysummer <udojyk@gmail.com>
2022-07-11 07:43:33 -0700

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, size;
+	hash_node_t *index_list;
+	int place_comma = 0;
+
+	size = ht->size;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (i = 0; i < size; i++)
+	{
+		index_list = ht->array[i];
+		while (index_list)
+		{
+			if (place_comma)
+				printf(", ");
+			printf("'%s' : '%s'", index_list->key, index_list->value);
+			index_list = index_list->next;
+			place_comma = 1;
+		}
+	}
+	printf("}\n");
+}
